72b83296d499643e45af99fc8cb01ab2f03042bb
bhautik17 <48475150+bhautik17@users.noreply.github.com>
2019-08-23 19:00:04 +0530

    Create sample_class_copy_constructor.cpp

---
+++ sample_class_copy_constructor.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class A
+{
+public:
+	A() {
+		cout << "default const" << endl;
+		num = new int;
+	}
+
+	A(const A &tmp) {
+		//A();
+		cout << "copy const" << endl;
+		num = new int;
+		*num = *tmp.num;
+	}
+	~A() {
+		cout << "destructor" << endl;
+	}
+	void setnum(int a) {
+		*num = a;
+	}
+	int getnum() {
+		return *num;
+	}
+private:
+	int *num;
+};
+int main()
+{
+	cout << "in Main" << endl;
+	A tmp;
+	tmp.setnum(20);
+	A tmp1 = tmp;
+	cout << "tmp1 num = "<<tmp1.getnum() << endl;
+	cout << "tmp num = " << tmp.getnum() << endl;
+	tmp1.setnum(30);
+	cout << "tmp1 num = " << tmp1.getnum() << endl;
+	cout << "tmp num = " << tmp.getnum() << endl;
+
+}
+
